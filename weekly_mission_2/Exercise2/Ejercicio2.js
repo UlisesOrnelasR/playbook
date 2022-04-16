f2e64008a75f760a5d46b69079f535e6eb00a3f8
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-04-15 20:26:32 -0600

    Resolviendo ejercicio 2 USO DE forEach

---
+++ weekly_mission_2/Exercise2/Ejercicio2.js
@@ -0,0 +1,69 @@
+console.log('--------Ejercicio 2---------')
+console.log('--------Uso de forEach en una lista---------')
+const explorers = [
+    {
+      name: "Explorer 1",
+      exercises_completed: 10,
+      rate: 99,
+      city: "CDMX",
+      stack: [
+        "js",
+        "c#"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: true,
+          exercisesFinished: true
+        }
+      }
+    },
+    {
+      name: "Explorer 2",
+      exercises_completed: 9,
+      city: "Veracruz",
+      rate: 50,
+      stack: [
+        "js"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: false,
+          exercisesFinished: false
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    },
+    {
+      name: "Explorer 3",
+      exercises_completed: 3,
+      city: "Sonora",
+      rate: 100,
+      stack: [
+        "elixir"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    }
+   ];
+
+   //forEach
+   //explorers.forEach(function(explorer){
+   //   console.log(explorer.name)
+   //})
+   // forEach con function arrow
+   explorers.forEach((explorer) => console.log(explorer.name))
