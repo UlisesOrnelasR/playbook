c2605a58e0b7de14e981b9f11fc1d89a0d51e504
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-04-15 23:04:47 -0600

    ejercicio 2 usando every

---
+++ weekly_mission_2/Exercise2/ejercicio2_every.js
@@ -0,0 +1,67 @@
+const explorers = [
+    {
+      name: "Explorer 1",
+      exercises_completed: 10,
+      rate: 99,
+      city: "CDMX",
+      stack: [
+        "js",
+        "c#"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: true,
+          exercisesFinished: true
+        }
+      }
+    },
+    {
+      name: "Explorer 2",
+      exercises_completed: 9,
+      city: "Veracruz",
+      rate: 50,
+      stack: [
+        "js"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: false,
+          exercisesFinished: false
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    },
+    {
+      name: "Explorer 3",
+      exercises_completed: 3,
+      city: "Sonora",
+      rate: 100,
+      stack: [
+        "elixir"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    }
+   ]
+   // Obtén la validación si todos los explorers tienen la propiedad isFinished del onboarding como true. Usa EVERY.
+   console.log('***************EJERCICIO 2**********************')
+   console.log('\n***************Uso de every**********************')
+
+   const onboardingFinished = explorers.every(explorer => explorer.missions.onboarding.isFinished == true)
+   console.log(onboardingFinished)
+
