85e1cc8921217a16f4c26a4013b14a4f2c21ac62
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-04-15 22:07:53 -0600

    ejercicio 2 usando FIND

---
+++ weekly_mission_2/Exercise2/ejercicio2_FIND.js
@@ -0,0 +1,74 @@
+const explorers = [
+    {
+      name: "Explorer 1",
+      exercises_completed: 10,
+      rate: 99,
+      city: "CDMX",
+      stack: [
+        "js",
+        "c#"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: true,
+          exercisesFinished: true
+        }
+      }
+    },
+    {
+      name: "Explorer 2",
+      exercises_completed: 9,
+      city: "Veracruz",
+      rate: 50,
+      stack: [
+        "js"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: false,
+          exercisesFinished: false
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    },
+    {
+      name: "Explorer 3",
+      exercises_completed: 3,
+      city: "Sonora",
+      rate: 100,
+      stack: [
+        "elixir"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    }
+   ]
+   // Busca el primer explorer que sea de la CDMX, usa FIND
+   console.log('**********EJERCICIO 2***********\n')
+   console.log('**********Uso de Find***********')
+
+   const explorerCDMX = explorers.find(function(explorer){
+    if (explorer.city == 'CDMX'){
+        return true
+    }
+   })
+   console.log(explorerCDMX)
+
+
+
+   
