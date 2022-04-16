0df82fdb9c4c4357845941f3b037af47e97261f3
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-04-15 22:58:03 -0600

    ejercicio 2 uso de SOME

---
+++ weekly_mission_2/Exercise2/ejercicio2_some.js
@@ -0,0 +1,71 @@
+const explorers = [
+    {
+      name: "Explorer 1",
+      exercises_completed: 10,
+      rate: 99,
+      city: "CDMX",
+      stack: [
+        "js",
+        "c#"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: true,
+          exercisesFinished: true
+        }
+      }
+    },
+    {
+      name: "Explorer 2",
+      exercises_completed: 9,
+      city: "Veracruz",
+      rate: 50,
+      stack: [
+        "js"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: false,
+          exercisesFinished: false
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    },
+    {
+      name: "Explorer 3",
+      exercises_completed: 3,
+      city: "Sonora",
+      rate: 100,
+      stack: [
+        "elixir"
+      ],
+      missions: {
+        onboarding: {
+          isFinished: true,
+          exercisesFinished: true
+        },
+        frontend: {
+          isFinished: false,
+          exercisesFinished: false
+        }
+      }
+    }
+   ]
+   //Obtén la validación si al menos uno de los explorers tiene la propiedad exercisesFinished en frontend como true, usa SOME
+    console.log('**************EJERCICIO 2***********')
+    console.log('**************Uso de Some***********')
+
+   const algunoTerminoFront = explorers.some(explorer => explorer.missions.frontend.exercisesFinished == true)
+   console.log(algunoTerminoFront)
+
+
+
+
+
